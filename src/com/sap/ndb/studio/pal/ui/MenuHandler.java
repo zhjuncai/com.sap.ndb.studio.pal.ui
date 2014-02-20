f6d98dc78ff99cef6468463752293338fab8ef93
Tank Zhao <Tank.zhao@sap.com>
2014-02-20 15:16:39 +0800

    initial package

---
+++ src/com/sap/ndb/studio/pal/ui/MenuHandler.java
@@ -0,0 +1,15 @@
+package com.sap.ndb.studio.pal.ui;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.*;
+
+public class MenuHandler extends AbstractHandler {
+	
+	public MenuHandler(){
+		
+	}
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		return null;
+	}
+
+}
---
+++ src/com/sap/ndb/studio/pal/ui/PALView.java
@@ -0,0 +1,23 @@
+package com.sap.ndb.studio.pal.ui;
+
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.ui.part.*;
+
+public class PALView extends ViewPart  {
+
+	private TableViewer viewer;
+	
+	
+	public PALView(){
+		
+	}
+	
+	public void createPartControl(final Composite parent) {
+		
+	}
+	
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+}
